09cbee2ac2c2429220299f9da8339ecee1063e8b
Sinabalar <sinabalar666@gmail.com>
2025-02-18 23:40:53 +0330

    refactor: improve navigation and layout structure
    - Updated `Navigation` component with responsive styling and hover effects
    - Extracted header into a separate `Header` component for better organization
    - Improved layout structure with flexbox for a full-height design
    - Added padding and max-width constraints to the main content area for better readability
    - Enhanced overall styling and responsiveness

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import Navigation from "@/app/_components/Navigation";
 import Logo from "@/app/_components/Logo";
 import "@/app/_styles/globals.css";
 import {Josefin_Sans} from "next/font/google";
+import Header from "@/app/_components/Header";
 
 const josefin = Josefin_Sans({
     subsets: ["latin"],
@@ -20,15 +21,13 @@ export default function RootLayout({children}) {
     return (
 
         <html lang={"en"}>
-        <body className={`${josefin.className} bg-primary-950 text-primary-100`}>
-        <header>
-            <Logo/>
-            <Navigation/>
-        </header>
-        <main>
-            {children}
-        </main>
-        <footer>Copyright by Wild Aura</footer>
+        <body className={`${josefin.className} bg-primary-950 text-primary-100 flex flex-col min-h-screen antialiased`}>
+        <Header/>
+        <div className={"flex-1 px-8 py-12"}>
+            <main className={" max-w-7xl mx-auto"}>
+                {children}
+            </main>
+        </div>
         </body>
         </html>
     );
