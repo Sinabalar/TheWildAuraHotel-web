c32133d62634b447c80e4a771a5a15550691f443
Sinabalar <sinabalar666@gmail.com>
2025-02-20 02:00:27 +0330

    feat: enhance account layout and navigation
    - Added `SideNavigation` component for account-related navigation
    - Created a layout for the account section with a sidebar and main content area
    - Updated `profile` and `reservations` pages with metadata and improved styling
    - Replaced `<a>` tags with `next/link` in `SideNavigation` for better routing
    - Improved overall layout and responsiveness in the root layout

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,3 @@
-import Navigation from "@/app/_components/Navigation";
-import Logo from "@/app/_components/Logo";
 import "@/app/_styles/globals.css";
 import {Josefin_Sans} from "next/font/google";
 import Header from "@/app/_components/Header";
@@ -25,8 +23,8 @@ export default function RootLayout({children}) {
             className={`${josefin.className} bg-primary-950 text-primary-100 flex flex-col min-h-screen antialiased relative`}
         >
         <Header/>
-        <div className={"flex-1 px-8 py-12"}>
-            <main className={" max-w-7xl mx-auto"}>
+        <div className={"flex-1 px-8 py-12 grid"}>
+            <main className={" max-w-7xl mx-auto w-full"}>
                 {children}
             </main>
         </div>
