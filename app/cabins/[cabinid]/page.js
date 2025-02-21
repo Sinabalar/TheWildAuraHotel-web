5d9ef56296558693c208e1b4c27c9127e2d89f18
Sinabalar <sinabalar666@gmail.com>
2025-02-22 02:54:23 +0330

    feat: add generateStaticParams for static cabin page generation
    - Added `generateStaticParams` to fetch cabins and generate static paths for each cabinid.
    - This enables pre-rendering of static pages for all cabins at build time.

--- app/cabins/[cabinid]/page.js
+++ app/cabins/[cabinid]/page.js
@@ -1,6 +1,6 @@
 import {EyeSlashIcon, MapPinIcon, UsersIcon} from "@heroicons/react/24/solid";
 import Image from "next/image";
-import {getCabin} from "@/app/_lib/data-service";
+import {getCabin, getCabins} from "@/app/_lib/data-service";
 
 export async function generateMetadata({params}) {
 
@@ -10,6 +10,12 @@ export async function generateMetadata({params}) {
         title: `Cabin ${name}`
     }
 
+}
+
+export async function generateStaticParams() {
+    const cabins = await getCabins();
+    const ids = cabins.map(cabin => ({cabinid:String(cabin.id)}))
+    return ids
 }
 
 export default async function Page({params}) {
