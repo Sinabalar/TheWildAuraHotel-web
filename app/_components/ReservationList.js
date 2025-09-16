1ae0f8728196244025978cab687fce0d8ab2681c
Sinabalar <sinabalar666@gmail.com>
2025-09-16 12:05:51 +0330

    feat: implement optimistic UI updates for reservation deletion

---
+++ app/_components/ReservationList.js
@@ -0,0 +1,29 @@
+"use client";
+
+import ReservationCard from "@/app/_components/ReservationCard";
+import {deleteReservation} from "@/app/_lib/actions";
+import {useOptimistic} from "react";
+
+export default function ReservationList({bookings}) {
+
+    const [optimisticBookings, optimisticDelete] = useOptimistic(bookings, (currBookings, bookingId) => {
+        return currBookings.filter((booking) => booking.id !== bookingId);
+    });
+
+    async function handleDelete(bookingId) {
+        optimisticDelete(bookingId)
+        await deleteReservation(bookingId);
+    }
+
+    return (
+        <ul className="space-y-6">
+            {optimisticBookings.map((booking) => (
+                <ReservationCard
+                    key={booking.id}
+                    onDelete={handleDelete}
+                    booking={booking}
+                />
+            ))}
+        </ul>
+    );
+}
